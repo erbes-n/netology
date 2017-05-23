e5380cd398e1493779fffa69b70b358d2352e34e
Nick Erbes <erbes-n@github.com>
2017-05-24 01:13:28 +0500

    Доработка задания 2.2 (исправлены отступы; использована функция dict.items)

--- Task_2.2/exercise_2-2.py
+++ Task_2.2/exercise_2-2.py
@@ -3,35 +3,35 @@ import yaml
 def open_cook_book_yml(file_name):
     with open(file_name, encoding = "utf8") as cb:
         new_cb = yaml.load(cb)
-        cook_book = {dish.lower().strip(): value for dish, value in zip(new_cb.keys(), new_cb.values())}
+        cook_book = {dish.lower().strip(): value for dish, value in new_cb.items()}
         for dish in cook_book:
             for i, ingr in enumerate(cook_book[dish]):
-                ingr = {key: value.lower().strip() for key, value in zip(ingr.keys(), ingr.values()) if key != "quantity"}
+                ingr = {key: value.lower().strip() for key, value in ingr.items() if key != "quantity"}
                 for key in ingr:
                     cook_book[dish][i][key] = ingr[key]
     return cook_book
 
 def get_shop_list_by_dishes(dishes, person_count, cook_book):
-  shop_list = {}
-  for dish in dishes:
-    for ingridient in cook_book[dish]:
-      new_shop_list_item = dict(ingridient)
-      new_shop_list_item['quantity'] *= person_count
-      if new_shop_list_item['ingridient_name'] not in shop_list:
-        shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
-      else:
-        shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
-  return shop_list
+    shop_list = {}
+    for dish in dishes:
+        for ingridient in cook_book[dish]:
+            new_shop_list_item = dict(ingridient)
+            new_shop_list_item['quantity'] *= person_count
+            if new_shop_list_item['ingridient_name'] not in shop_list:
+                shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+            else:
+                shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
+    return shop_list
 
 def print_shop_list(shop_list):
-  for shop_list_item in shop_list.values():
-    print('{ingridient_name} {quantity} {measure}'.format(**shop_list_item))
+    for shop_list_item in shop_list.values():
+        print('{ingridient_name} {quantity} {measure}'.format(**shop_list_item))
 
 def create_shop_list(cook_book):
-  person_count = int(input('Введите количество человек: '))
-  dishes = input('Введите количество блюд в расчете на одного (через запятую): ').strip().lower().split(', ')
-  shop_list = get_shop_list_by_dishes(dishes, person_count, cook_book)
-  print_shop_list(shop_list)
+    person_count = int(input('Введите количество человек: '))
+    dishes = input('Введите количество блюд в расчете на одного (через запятую): ').strip().lower().split(', ')
+    shop_list = get_shop_list_by_dishes(dishes, person_count, cook_book)
+    print_shop_list(shop_list)
 
 cook_book = open_cook_book_yml("cook_book.yml")
 create_shop_list(cook_book)
